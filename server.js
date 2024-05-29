9cb1d78415ca3cb908cddff88bd9c1ae64c32d56
3steban96 <andersonrodriguez748@gmail.com>
2024-05-29 12:34:05 -0500

    update cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-  origin: '*',
+  origin: ['https://www.checkmyhardware.cloud/','https://www.checkmyhardware.cloud','https://checkmyhardware.cloud','https://checkmyhardware.cloud/'],
   credentials: true,
   methods: 'GET',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, x-auth-token',
